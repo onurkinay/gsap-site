4d4af7146b7a0c88fe4e178fc152dbb0cd924be3
Onur KINAY <ben@onurkinay.com.tr>
2025-09-03 16:54:40 +0300

    tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
-import React from "react";
+import { ScrollTrigger, SplitText } from "gsap/all";
+import gsap from "gsap";
+
+gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="flex-center h-[100vh]">
+      <h1 className="text-3xl font-bold">Hello, World!</h1>
+    </div>
+  );
 };
 
 export default App;
