9d87e049fc881a036e8bf21a9885071cf673052d
Onur KINAY <ben@onurkinay.com.tr>
2025-09-03 17:30:35 +0300

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { ScrollTrigger, SplitText } from "gsap/all";
 import gsap from "gsap";
+import Navbar from "./components/Navbar";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
   return (
-    <div className="flex-center h-[100vh]">
-      <h1 className="text-3xl font-bold">Hello, World!</h1>
-    </div>
+    <main>
+      <Navbar />
+    </main>
   );
 };
 
